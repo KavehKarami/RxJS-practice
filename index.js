461533a87922b996b44c08948a3e0ff6532604b6
Kaveh Karami <kaveh.karami.kk@gmail.com>
2020-08-13 23:44:32 +0430

    end of rxjs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import { fromEvent, Observable, from } from "rxjs";
-import { scan, throttleTime, map } from "rxjs/operators";
+/* 
+  NOTE ma be 2 model mitoonim Observable besazim 
+  1: new konim Observable ro
+  2: yekseri method hastan mesle (of, from, Subject, ...) ke built-in new Observable misazan
+*/
+
+import { fromEvent, Observable, Subject, from, BehaviorSubject } from "rxjs";
+import { scan, throttleTime, map, multicast } from "rxjs/operators";
 
 const button = document.querySelector("button");
-
-/* -------------------------------------------------------------------------- */
-/* ------------------------------ RxJS Overview ----------------------------- */
-/* -------------------------------------------------------------------------- */
 
 // fromEvent(button, "click")
 //   .pipe(scan((count) => count + 1, 0))
@@ -60,90 +62,100 @@ const button = document.querySelector("button");
 
 // with Rxjs
 
-fromEvent(button, "click")
-  .pipe(
-    throttleTime(1000),
-    map((event) => event.ctrlKey),
-    scan((count, ctrlKey) => {
-      if (ctrlKey) {
-        return count + 1;
-      } else {
-        return count;
-      }
-    }, 0)
-  )
-  .subscribe((count) => (document.querySelector("h4").innerText = `${count}`));
+// fromEvent(button, "click")
+//   .pipe(
+//     throttleTime(1000),
+//     map((event) => event.ctrlKey),
+//     scan((count, ctrlKey) => {
+//       if (ctrlKey) {
+//         return count + 1;
+//       } else {
+//         return count;
+//       }
+//     }, 0)
+//   )
+//   .subscribe((count) => (document.querySelector("h4").innerText = `${count}`));
 
 /* -------------------------------------------------------------------------- */
 
-/* -------------------------------------------------------------------------- */
-/* -------------------------- End of Rxjs Overview -------------------------- */
-/* -------------------------------------------------------------------------- */
+// const observable = new Observable((subscriber) => {
+//   try {
+//     subscriber.next(1);
+//     subscriber.next(2);
+//     subscriber.next(3);
 
-/* -------------------------------------------------------------------------- */
-/* ---------------------------- RxJS Observables ---------------------------- */
-/* -------------------------------------------------------------------------- */
+//     setTimeout(() => {
+//       subscriber.next(4);
+//       // subscriber.complete();
+//     }, 1000);
 
-const observable = new Observable((subscriber) => {
-  try {
-    subscriber.next(1);
-    subscriber.next(2);
-    subscriber.next(3);
+//     subscriber.next(5);
 
-    setTimeout(() => {
-      subscriber.next(4);
-      // subscriber.complete();
-    }, 1000);
+//     const intervalId = setInterval(() => {
+//       subscriber.next("hi");
+//     }, 1000);
 
-    subscriber.next(5);
+//     // we can also unsubscribe() that without this code below but it's a custom
+//     return function unsubscribe() {
+//       console.log("unsubcribe our observables");
+//       clearInterval(intervalId);
+//     };
+//   } catch (err) {
+//     subscriber.error(err);
+//   }
+// });
 
-    const intervalId = setInterval(() => {
-      subscriber.next("hi");
-    }, 1000);
-
-    // we can also unsubscribe() that without this code below but it's a custom
-    return function unsubscribe() {
-      console.log("unsubcribe our observables");
-      clearInterval(intervalId);
-    };
-  } catch (err) {
-    subscriber.error(err);
-  }
-});
-
-console.log("just before subscribe");
+// console.log("just before subscribe");
 
 // if we want to use unsubsribe(), we gotta store our observable.subscribe(...) in a variable
-const useObservable = observable.subscribe({
-  next(x) {
-    console.log("got value " + x);
-  },
-  error(err) {
-    console.log(err);
-  },
-  complete() {
-    console.log("done");
-  },
-});
 
-console.log("just after subscribe");
+// const useObservable = observable.subscribe({
+//   next(x) {
+//     console.log("got value " + x);
+//   },
+//   error(err) {
+//     console.log(err);
+//   },
+//   complete() {
+//     console.log("done");
+//   },
+// });
+
+// console.log("just after subscribe");
 
 /* -------------------------------------------------------------------------- */
 
 // use unsubscribe()
-fromEvent(button, "click").subscribe(() => {
-  useObservable.unsubscribe();
+
+// fromEvent(button, "click").subscribe(() => {
+//   useObservable.unsubscribe();
+// });
+
+// const foo = new Observable((subscriber) => {
+//   console.log("Hello");
+//   subscriber.next(42);
+// });
+
+// foo.subscribe((x) => console.log(x));
+
+/* -------------------------------------------------------------------------- */
+
+// const subject = new Subject();
+// const subject = new BehaviorSubject("helloooooo");
+// // hi dosn't appear and execute
+// subject.next("hi");
+// subject.subscribe((v) => console.log(v));
+// subject.next("bye");
+
+/* -------------------------------------------------------------------------- */
+
+const source = from([1, 2, 3, 4]);
+const subject = new Subject();
+const multicasted = source.pipe(multicast(subject));
+multicasted.subscribe({
+  next: (v) => console.log(`observerA: ${v}`),
 });
-
-const foo = new Observable((subscriber) => {
-  console.log("Hello");
-  subscriber.next(42);
+multicasted.subscribe({
+  next: (v) => console.log(`observerB: ${v}`),
 });
-
-foo.subscribe((x) => console.log(x));
-
-/* -------------------------------------------------------------------------- */
-
-/* -------------------------------------------------------------------------- */
-/* ------------------------- End of Rxjs Observables ------------------------ */
-/* -------------------------------------------------------------------------- */
+multicasted.connect();
